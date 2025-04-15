a0cd205a656888ffffbf6862ec3fdbe8a749179b
C0A24206 <c0a24206ea@edu.teu.ac.jp>
2025-04-15 15:44:31 +0900

    FPS変更

--- pygame_image.py
+++ pygame_image.py
@@ -10,17 +10,18 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    kokaton_img = pg.transform.flip(kokaton_img, True, False)
+    tori_img = pg.image.load("fig/3.png")
+    tori_img = pg.transform.flip(tori_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kokaton_img, [300,200])
+        screen.blit(tori_img, [300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
